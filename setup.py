bc16ac4b4a5b5c715326636433195f78834546a7
arnaud CAZELLES <a.cazelles@gmail.com>
2021-06-28 19:27:02 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(
+    name='bankup',
+    version='0.0.1',
+    description='package from project which aims is to manage budget',
+    url='git@github.com:arnaudczls/bankup.git',
+    author='Arnaud CAZELLES',
+    author_email='a.cazelles@gmail.com',
+    license='unlicense',
+    packages=find_packages(),
+    install_requires=requirements,
+    packages=['bankup'],
+    zip_safe=False
+)
